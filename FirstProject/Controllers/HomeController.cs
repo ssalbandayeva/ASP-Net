f631ecd865101a060013fce579686851ed4552f9
ssalbandayeva <samrasa@code.edu.az>
2023-05-09 18:17:52 +0400

    practise all

---
+++ FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "P327 Home Page";
+        }
+
+        public string About()
+        {
+            return "P327 About Page";
+        }
+
+        public string Detail(int? id)
+        {
+            return $"P327 Student id: {id}";
+        }
+    }
+}
---
+++ FirstProject/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Index()
+        {
+            List<Student> students = new()
+            {
+                new Student{Id=1,Name="Mehemmed",Age=27},
+                new Student{Id=2,Name="Shahin",Age=18},
+                new Student{Id=3,Name="Huseyn",Age=21},
+                new Student{Id=4,Name="Orxan",Age=26},
+                new Student{Id=5,Name="Semra",Age=32}
+            };
+
+            
+            //return View();
+            return View(students);
+            
+        }
+                
+        //public IActionResult Detail(int? id)
+        //{
+        //    return View(id);
+        //}
+
+        public IActionResult Detail(int? id)
+        {
+            ViewBag.Id = id;
+
+            return View();
+        }
+    }
+}
